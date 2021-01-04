c8fdecc7db06e210ca4794dc575439086d41462f
David <david@noemail.com>
2021-01-04 23:53:35 +0100

    Change how files are read to avoid "too many files open" error

--- client/main.go
+++ client/main.go
@@ -48,9 +48,13 @@ func check(e error) {
 }
 
 func readFile(path string) string {
-	dat, err := ioutil.ReadFile(path)
+	file, err := os.Open(path)
 	check(err)
-	return string(dat)
+	content := make([]byte, 1024)
+	numBytes, err := file.Read(content)
+	check(err)
+	file.Close()
+	return string(content[:numBytes])
 }
 
 func getHostname() string {
@@ -91,24 +95,24 @@ func calculateCPUUsage() {
 }
 
 func readCPUStats() (idle, total uint64) {
-	rawRamStats := readFile("/proc/stat")
-	lines := strings.Split(rawRamStats, "\n")
-	for _, line := range(lines) {
-        fields := strings.Fields(line)
-        if fields[0] == "cpu" {
-            numFields := len(fields)
-            for i := 1; i < numFields; i++ {
-                val, err := strconv.ParseUint(fields[i], 10, 64)
-                if err != nil {
-                    log.Println("Error: ", i, fields[i], err)
-                }
-                total += val // tally up all the numbers to get total ticks
-                if i == 4 {  // idle is the 5th field in the cpu line
-                    idle = val
-                }
-            }
-            return
-        }
+	rawCPUStats := readFile("/proc/stat")
+	lines := strings.Split(rawCPUStats, "\n")
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		if fields[0] == "cpu" {
+			numFields := len(fields)
+			for i := 1; i < numFields; i++ {
+				val, err := strconv.ParseUint(fields[i], 10, 64)
+				if err != nil {
+					log.Println("Error: ", i, fields[i], err)
+				}
+				total += val // tally up all the numbers to get total ticks
+				if i == 4 {  // idle is the 5th field in the cpu line
+					idle = val
+				}
+			}
+			return
+		}
 	}
 	return
 }
